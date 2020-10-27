77ba053de2074a5a8413d8e04d5d54480961d7fc
Hemlata Mahaur <hemlatamahaur96@gmail.com>
2020-10-27 17:09:43 +1000

    Update foobar.php

--- foobar.php
+++ foobar.php
@@ -4,15 +4,14 @@ for ($i = 1; $i<=100; $i++) {
     if ($i % 3 && $i % 5) {
         echo $i;
     } else {
-        if ($i % 3 == 0) {
-            echo "foo";
-        }
-        if ($i % 5 == 0) {
-            echo "bar";
-        }
-        if ($i % 3 == 0 && $i % 5 == 0) {
-            echo "foobar\r\n";
-        }
+
+if ($i % 3 == 0) {
+        echo "foo";
+    } elseif ($i % 5 == 0) {
+        echo "bar";
+    } elseif ($i % 3 == 0 && $i % 5 == 0) {
+        echo "foobar\r\n";
+    }   
     }
     //line breaks to enhance output readability
     echo "\r\n";
