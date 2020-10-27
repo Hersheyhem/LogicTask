cbcb866213ccb45d82063f6de90c3bccef3744af
Hemlata Mahaur <hemlatamahaur96@gmail.com>
2020-10-27 16:59:32 +1000

    Add files via upload

---
+++ foobar.php
@@ -0,0 +1,21 @@
+<?php
+
+for ($i = 1; $i<=100; $i++) {
+    if ($i % 3 && $i % 5) {
+        echo $i;
+    } else {
+        if ($i % 3 == 0) {
+            echo "foo";
+        }
+        if ($i % 5 == 0) {
+            echo "bar";
+        }
+        if ($i % 3 == 0 && $i % 5 == 0) {
+            echo "foobar\r\n";
+        }
+    }
+    //line breaks to enhance output readability
+    echo "\r\n";
+}
+
+?>
